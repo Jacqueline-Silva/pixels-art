fc8001630b13d9e59f46e7e65a8acc88e3b4a5f2
Jacqueline Silva <jacque.sx@hotmail.com>
2021-12-09 22:04:40 -0300

    corrigindo excesso de erros github

--- script.js
+++ script.js
@@ -1,52 +1,49 @@
 window.onload = function () {
-    let corPretaAoIniciar = document.querySelector('.color');
-    corPretaAoIniciar.classList.add('selected');
+  const corPretaAoIniciar = document.querySelector('.color');
+  corPretaAoIniciar.classList.add('selected');
+};
+
+const quadrado1 = document.getElementsByClassName('color')[0];
+quadrado1.style.background = 'black';
+
+const quadrado2 = document.getElementsByClassName('color')[1];
+quadrado2.style.background = 'pink';
+
+const quadrado3 = document.getElementsByClassName('color')[2];
+quadrado3.style.background = 'yellow';
+
+const quadrado4 = document.getElementsByClassName('color')[3];
+quadrado4.style.background = 'lightgreen';
+
+function selecionaCor(selecionando) {
+  const seleciona = document.querySelector('.selected');
+  seleciona.classList.remove('selected');
+  selecionando.target.classList.add('selected');
 }
 
-let quadrado1 = document.getElementsByClassName('color')[0];
-quadrado1.style.background = "black"
+quadrado1.addEventListener('click', selecionaCor);
+quadrado2.addEventListener('click', selecionaCor);
+quadrado3.addEventListener('click', selecionaCor);
+quadrado4.addEventListener('click', selecionaCor);
 
-let quadrado2 = document.getElementsByClassName('color')[1];
-quadrado2.style.background = "pink"
+const pixelUm = document.getElementById('pixel-um');
+const pixelDois = document.getElementById('pixel-dois');
+const pixelTres = document.getElementById('pixel-tres');
+const pixelQuatro = document.getElementById('pixel-quatro');
+const pixelCinco = document.getElementById('pixel-cinco');
 
-let quadrado3 = document.getElementsByClassName('color')[2];
-quadrado3.style.background = "yellow"
-
-let quadrado4 = document.getElementsByClassName('color')[3];
-quadrado4.style.background = "lightgreen"
-
-
-function selecionaCor (selecionando) {
-    let seleciona = document.querySelector ('.selected')
-    seleciona.classList.remove('selected')
-    selecionando.target.classList.add ('selected')
+function colorindo(select) {
+  const colocandoCor = document.querySelector('.selected');
+  select.target.style.backgroundColor = colocandoCor.style.backgroundColor;
 }
+pixelUm.addEventListener('click', colorindo);
+pixelDois.addEventListener('click', colorindo);
+pixelTres.addEventListener('click', colorindo);
+pixelQuatro.addEventListener('click', colorindo);
+pixelCinco.addEventListener('click', colorindo);
 
-quadrado1.addEventListener ('click', selecionaCor)
-quadrado2.addEventListener ('click', selecionaCor) 
-quadrado3.addEventListener ('click', selecionaCor) 
-quadrado4.addEventListener ('click', selecionaCor) 
-
-
-let pixelUm = document.getElementById('pixel-um');
-let pixelDois = document.getElementById('pixel-dois');
-let pixelTres = document.getElementById('pixel-tres');
-let pixelQuatro = document.getElementById('pixel-quatro');
-let pixelCinco = document.getElementById('pixel-cinco');
-
-function colorindo (select) {
-    let colocandoCor = document.querySelector ('.selected')
-    select.target.style.backgroundColor = colocandoCor.style.backgroundColor
+const chamandoBotao = document.getElementById('clear-board');
+function clear(limpando) {
+  limpando.target = window.location.reload();
 }
-pixelUm.addEventListener ('click', colorindo)
-pixelDois.addEventListener ('click', colorindo)
-pixelTres.addEventListener ('click', colorindo)
-pixelQuatro.addEventListener ('click', colorindo)
-pixelCinco.addEventListener ('click', colorindo)
-
-
-let chamandoBotao = document.getElementById('clear-board')
-function clear (limpando) {
- limpando.target = window.location.reload()
-}
-chamandoBotao.addEventListener ('click', clear)
+chamandoBotao.addEventListener('click', clear);
